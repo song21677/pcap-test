ee813a2d560a7b3468363da41ddc26951617d1a1
sy99 <song21677@naver.com>
2020-04-07 00:01:26 +0900

    Modified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <headers.h>
+#include <algorithm>
+using namespace std;
+
+#define ETHERTYPE_IP 0X0800
 
 void usage() {
     printf("syntax: pcap-test <interface>\n");
     printf("sample: pcap-test wlan0\n");
 }
 
-void print_ethernet(uint8_t const ethernet_mac[6]) {
+void print_mac(uint8_t const ethernet_mac[6]) {
     for(int i=0; i<6; i++) {
     printf("%02x", ethernet_mac[i]);
         if(i != 5) {
@@ -44,8 +48,7 @@ int main(int argc, char* argv[]) {
         const u_char* packet;
 
         int res = pcap_next_ex(handle, &header, &packet);
-        if (res == 0)
-            continue;
+        if (res == 0) continue;
         if (res == -1 || res == -2) {
             printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
             break;
@@ -53,38 +56,38 @@ int main(int argc, char* argv[]) {
         printf("%u bytes captured\n", header->caplen);
 
         const struct ethernet_header* ethernet = (struct ethernet_header*)(packet);
-        if (ntohs(ethernet->ethernet_type) == 0x0800) {
-            const struct ip_header* ip = (struct ip_header*)(packet + sizeof(ethernet_header));
-            int ip_hl = (ip->ip_vhl & 0x0F)*4;
-            if (ip->ip_pi == 6) {
-                const struct tcp_header* tcp = (struct tcp_header*)(packet + sizeof(ethernet_header) + ip_hl);
-                int tcp_hl = (((tcp->tcp_or & 0xF0)>>4)*4);
-                const u_char *payload = packet + sizeof(ethernet_header) + ip_hl + tcp_hl;
-                int payload_length = ntohs(ip->ip_tpl)-ip_hl-tcp_hl;
+        if (ntohs(ethernet->ethernet_type) != ETHERTYPE_IP) continue;
 
-                printf("src mac: ");
-                print_ethernet(ethernet->ethernet_smac);
-                printf(", dst mac: ");
-                print_ethernet(ethernet->ethernet_dmac);
-                printf("\n\n");
+        const struct ip_header* ip = (struct ip_header*)(packet + sizeof(ethernet_header));
+        int ip_hl = (ip->ip_vhl & 0x0F)*4;
+        if (ip->ip_pi != IPPROTO_TCP) continue;
 
-                printf("src ip address: ");
-                print_ip(ip->ip_src);
-                printf(", dst ip address: ");
-                print_ip(ip->ip_dst);
-                printf("\n\n");
+        const struct tcp_header* tcp = (struct tcp_header*)(packet + sizeof(ethernet_header) + ip_hl);
+        int tcp_hl = (((tcp->tcp_or & 0xF0)>>4)*4);
 
-                printf("src port: %d, dst port: %d\n\n", htons(tcp->tcp_sport), htons(tcp->tcp_dport));
+        const u_char *payload = packet + sizeof(ethernet_header) + ip_hl + tcp_hl;
+        int payload_length = ntohs(ip->ip_tpl)-ip_hl-tcp_hl;
 
-                for (int i=1; i<=payload_length; i++) {
-                    printf("%02x ", *payload);
-                    payload++;
-                    if (i>15) break;
-                }
-            printf("\n\n");
-            }
-        }
+        printf("src mac: ");
+        print_mac(ethernet->ethernet_smac);
+        printf(", dst mac: ");
+        print_mac(ethernet->ethernet_dmac);
+        printf("\n\n");
+
+        printf("src ip address: ");
+        print_ip(ip->ip_src);
+        printf(", dst ip address: ");
+        print_ip(ip->ip_dst);
+        printf("\n\n");
+
+        printf("src port: %d, dst port: %d\n\n", ntohs(tcp->tcp_sport), ntohs(tcp->tcp_dport));
+
+        int print_length = min(payload_length, 16);
+        for (int i=1; i<=print_length; i++) {
+            printf("%02x ", *payload);
+            payload++;
+         }
+         printf("\n\n");
     }
-
     pcap_close(handle);
 }
